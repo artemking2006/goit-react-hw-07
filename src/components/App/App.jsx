abc20dd0d8888e6d68ee6997916b8e1033479425
Artem Zakharchuk <zakharchukartem18@gmail.com>
2025-05-16 20:54:34 +0300

    New change!

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,14 +6,18 @@ import s from "./App.module.css";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchContacts } from "../../redux/contactsOps";
 import { selectContacts } from "../../redux/contactsSlice";
+import { selectLoading } from "../../redux/contactsSlice";
 
 const App = () => {
     const dispatch = useDispatch();
     const contacts = useSelector(selectContacts);
+    const loading = useSelector(selectLoading);
     
     useEffect(() => {
-        if (contacts.length === 0) dispatch(fetchContacts());
-    }, [dispatch, contacts]);
+        if (!loading && contacts.length === 0) {
+            dispatch(fetchContacts());
+        }
+    }, [dispatch, loading, contacts]);
 
     return (
         <ul className={s.list}>
